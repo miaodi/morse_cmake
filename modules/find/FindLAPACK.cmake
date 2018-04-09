f0165e88f7f37a8a7fa6d8be26c36e661cd2e2e7
Florent Pruvost <florent.pruvost@inria.fr>
2018-04-09 12:03:41 +0200

    find_pkgconfig_libraries_absolute_path macro is now factorized in a morse module file

--- modules/find/FindLAPACK.cmake
+++ modules/find/FindLAPACK.cmake
@@ -3,7 +3,7 @@
 # @copyright (c) 2009-2014 The University of Tennessee and The University
 #                          of Tennessee Research Foundation.
 #                          All rights reserved.
-# @copyright (c) 2012-2016 Inria. All rights reserved.
+# @copyright (c) 2012-2018 Inria. All rights reserved.
 # @copyright (c) 2012-2014 Bordeaux INP, CNRS (LaBRI UMR 5800), Inria, Univ. Bordeaux. All rights reserved.
 #
 ###
@@ -320,7 +320,7 @@ if(BLAS_FOUND)
     find_library(M_LIBRARY NAMES m)
     mark_as_advanced(M_LIBRARY)
     if(M_LIBRARY)
-      set(LM "-lm")
+      set(LM "${M_LIBRARY}")
     else()
       set(LM "")
     endif()
@@ -365,44 +365,6 @@ if(BLAS_FOUND)
     set(LAPACK_GIVEN_BY_USER "TRUE")
   endif()
 
-  macro(FIND_PKGCONFIG_LIBRARIES_ABSOLUTE_PATH _prefix)
-    if(WIN32)
-      string(REPLACE ":" ";" _lib_env "$ENV{LIB}")
-    elseif(APPLE)
-      string(REPLACE ":" ";" _lib_env "$ENV{DYLD_LIBRARY_PATH}")
-    else()
-      string(REPLACE ":" ";" _lib_env "$ENV{LD_LIBRARY_PATH}")
-    endif()
-    list(APPEND _lib_env "${CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES}")
-    list(APPEND _lib_env "${CMAKE_C_IMPLICIT_LINK_DIRECTORIES}")
-    set(${_prefix}_LIBRARIES_COPY "${${_prefix}_LIBRARIES}")
-    set(${_prefix}_LIBRARIES "")
-    foreach(_library ${${_prefix}_LIBRARIES_COPY})
-        get_filename_component(_library "${_library}" NAME_WE)
-        find_library(_library_path NAMES ${_library}
-            HINTS ${${_prefix}_LIBDIR} ${${_prefix}_LIBRARY_DIRS} ${_lib_env})
-        if (_library_path)
-            list(APPEND ${_prefix}_LIBRARIES ${_library_path})
-        else()
-            message(FATAL_ERROR "Dependency of ${_prefix} '${_library}' NOT FOUND")
-        endif()
-        unset(_library_path CACHE)
-    endforeach()
-    set(${_prefix}_STATIC_LIBRARIES_COPY "${${_prefix}_STATIC_LIBRARIES}")
-    set(${_prefix}_STATIC_LIBRARIES "")
-    foreach(_library ${${_prefix}_STATIC_LIBRARIES_COPY})
-        get_filename_component(_library "${_library}" NAME_WE)
-        find_library(_library_path NAMES ${_library}
-            HINTS ${${_prefix}_STATIC_LIBDIR} ${${_prefix}_STATIC_LIBRARY_DIRS} ${_lib_env})
-        if (_library_path)
-            list(APPEND ${_prefix}_STATIC_LIBRARIES ${_library_path})
-        else()
-            message(FATAL_ERROR "Dependency of ${_prefix} '${_library}' NOT FOUND")
-        endif()
-        unset(_library_path CACHE)
-    endforeach()
-  endmacro()
-  
   # Optionally use pkg-config to detect include/library dirs (if pkg-config is available)
   # -------------------------------------------------------------------------------------
   include(FindPkgConfig)

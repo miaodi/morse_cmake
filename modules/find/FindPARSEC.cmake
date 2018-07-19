cb048ebffc601f163b81d847022d7c2a2e59e27e
Florent Pruvost <florent.pruvost@inria.fr>
2018-07-19 16:25:04 +0200

    parsec is the only lib required now

--- modules/find/FindPARSEC.cmake
+++ modules/find/FindPARSEC.cmake
@@ -399,7 +399,7 @@ if( (NOT PKG_CONFIG_EXECUTABLE) OR (PKG_CONFIG_EXECUTABLE AND NOT PARSEC_FOUND) 
   # ----------------------------------------------
 
   # create list of libs to find
-  set(PARSEC_libs_to_find "parsec" "parsec-base" "parsec_distribution" "parsec_distribution_matrix")
+  set(PARSEC_libs_to_find "parsec")
 
   # call cmake macro to find the lib path
   if(PARSEC_LIBDIR)

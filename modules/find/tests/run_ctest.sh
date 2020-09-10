a39e11eed7ff1cc83b5ef771f9fad3632de43749
Florent Pruvost <florent.pruvost@inria.fr>
2020-09-10 11:13:16 +0200

    Add a test script

---
+++ modules/find/tests/run_ctest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# env. var. CI_PROJECT_DIR required
+[ -z "$CI_PROJECT_DIR" ] && echo "Variable CI_PROJECT_DIR must be set to execute this script." && exit 1
+
+mkdir -p build
+cd build
+
+cmake $CI_PROJECT_DIR/modules/find/tests -DENABLE_CTEST=ON -DLAPACKE_COMPONENTS="TMG" -DQUARK_COMPONENTS="HWLOC" -DCHAMELEON_COMPONENTS="MPI;FXT;STARPU" -DPASTIX_COMPONENTS="SPM"
+
+ctest -V
+
+exit 0
